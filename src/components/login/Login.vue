e3ccbf8dfb3b5f856f6be15301fdadf1c218d942
328295484@qq.com <wht@123456>
2020-03-27 16:52:30 +0800

    Initial commit

---
+++ src/components/login/Login.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="login" :style="'height:'+fullHeight+'px;'">
+    <el-container direction="vertical">
+      <el-header>
+          <img src="./static/imgs/login_title.png" class="imgs">
+      </el-header>
+      <el-main>
+        <el-form ref="form" :model="form">
+          <el-form-item prop="name">
+            <el-col :span="4" :offset="10">
+              <el-input v-model="form.name" placeholder="用户名" prefix-icon="el-icon-user" clearable></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item prop="pwd">
+            <el-col :span="4" :offset="10">
+              <el-input v-model="form.pwd" type="password" placeholder="密码" prefix-icon="el-icon-lock" clearable></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+            <el-col :span="5" :offset="8">
+              <el-checkbox label="记住密码"></el-checkbox>
+            </el-col>
+          </el-form-item>
+          <el-form-item>
+              <el-button type="success">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-main>
+      <el-footer>
+        <img src="./static/imgs/login_bottom.png" class="imgs"/>
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Login",
+    data() {
+      return {
+        form:{
+          name:'',
+          pwd:'',
+        },
+        fullHeight: document.documentElement.clientHeight //获取屏幕高度
+      }
+    },
+    watch:{
+      fullHeight(val){ //监控浏览器高度
+        if (!this.timer){
+          this.fullHeight = val;
+          this.timer = true;
+          let that = this;
+          setTimeout(function (){
+            that.timer = false
+          },400)
+        }
+      }
+    },
+    mounted () {
+      this.get_bodyHeight()
+    },
+    methods :{
+      get_bodyHeight () {//动态获取浏览器高度
+        const that = this
+        window.onresize = () => {
+          return (() => {
+            window.fullHeight = document.documentElement.clientHeight
+            that.fullHeight = window.fullHeight
+          })()
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .login {
+    width: 100%;
+    background: url("./static/imgs/bg-24.png") no-repeat center;
+    background-size: 100% 100%;
+  }
+
+  .el-header {
+    margin-top: 10%;
+  }
+
+  .el-main {
+    margin-top: 8%;
+  }
+
+  .el-footer {
+    margin-top: 5%;
+  }
+
+  .el-button {
+    width: 16.5%;
+  }
+
+  .imgs {
+    position: relative;
+  }
+
+</style>
